caa3d0b185467bca62ca40a684a595e5a44dec9a
gjgustjd <gjgustjd70@naver.com>
2022-04-07 03:07:45 +0900

    Feat:ImageRepository에 의존성 주입 적용
    의존성 주입 적용
    기존 getImageList 메소드에 콜백 파라미터 추가

--- app/src/main/java/com/miso/showimage/model/ImageRepository.kt
+++ app/src/main/java/com/miso/showimage/model/ImageRepository.kt
@@ -7,9 +7,17 @@ import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Inject
+import javax.inject.Singleton
 
-object ImageRepository {
-    fun getImageList(page: Int = 2, limit: Int = 100) {
+@Singleton
+class ImageRepository @Inject constructor() {
+    fun getImageList(
+        page: Int = 2,
+        limit: Int = 100,
+        onResponse: (Call<List<ImageDto>>, Response<List<ImageDto>>) -> Unit,
+        onFailure: (Call<List<ImageDto>>, Throwable) -> Unit
+    ) {
         var gson = GsonBuilder().setLenient().create()
 
         val retrofit = Retrofit.Builder()
@@ -23,10 +31,12 @@ object ImageRepository {
                 response: Response<List<ImageDto>>
             ) {
                 Log.i("getImageList", "onResponse")
+                onResponse(call, response)
             }
 
             override fun onFailure(call: Call<List<ImageDto>>, t: Throwable) {
                 Log.i("getImageList", "onFailure")
+                onFailure(call, t)
             }
         }
         )
