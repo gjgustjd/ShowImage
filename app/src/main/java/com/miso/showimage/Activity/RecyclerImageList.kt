d4f427e242a042e369b07187cf1be3dbddb13c4c
gjgustjd <gjgustjd70@naver.com>
2022-04-09 14:56:29 +0900

    Feat:RecyclerImageList 추가
    이미지 표시용 RecyclerImageList.kt 추가

---
+++ app/src/main/java/com/miso/showimage/Activity/RecyclerImageList.kt
@@ -0,0 +1,30 @@
+package com.miso.showimage.Activity
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.miso.showimage.databinding.ItemImageBinding
+import com.miso.showimage.model.ImageDto
+
+class RecyclerImageList(private val imageData: List<ImageDto>) :
+    RecyclerView.Adapter<RecyclerImageList.Holder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+        val inflater = LayoutInflater.from(parent.context)
+        val itemImageBinding = ItemImageBinding.inflate(inflater, parent, false)
+        return Holder(itemImageBinding)
+    }
+
+    override fun onBindViewHolder(holder: Holder, position: Int) {
+        holder.bind(imageData[position])
+    }
+
+    override fun getItemCount(): Int {
+        return imageData.size
+    }
+
+    inner class Holder(val binding: ItemImageBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(imageDto: ImageDto) {
+            binding.imageDto = imageDto
+        }
+    }
+}
