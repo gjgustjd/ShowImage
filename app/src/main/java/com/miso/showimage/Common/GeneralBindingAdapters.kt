996611d2f2e90ed26a1e90dd66e365e2519bff66
gjgustjd <gjgustjd70@naver.com>
2022-04-09 17:21:50 +0900

    Feat:이미지뷰 비트맵으로 로드
    ImageView에 할당된 imageUrl로 다운로드 시 비트맵으로 표시
    한 번 다운로드 된 이미지 뷰는 다시 다운로드하지 않도록 변경

--- app/src/main/java/com/miso/showimage/Common/GeneralBindingAdapters.kt
+++ app/src/main/java/com/miso/showimage/Common/GeneralBindingAdapters.kt
@@ -1,19 +1,55 @@
 package com.miso.showimage.Common
 
+import android.graphics.Bitmap
+import android.util.Log
+import android.view.View
 import android.widget.ImageView
+import androidx.annotation.Nullable
+import androidx.core.view.isGone
+import androidx.core.view.isInvisible
+import androidx.core.view.isVisible
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
 import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.RequestOptions
+import com.bumptech.glide.request.target.Target
 
 object GeneralBindingAdapters {
     @BindingAdapter("app:imageUrl")
     @JvmStatic
     fun loadImage(imageView: ImageView, url: String) {
-        Glide.with(imageView.context)
-            .load(url)
-            .diskCacheStrategy(DiskCacheStrategy.NONE)
-            .apply(RequestOptions().fitCenter())
-            .into(imageView)
+        if(imageView.isInvisible) {
+            Glide.with(imageView.context)
+                .asBitmap()
+                .load(url)
+                .skipMemoryCache(true)
+                .diskCacheStrategy(DiskCacheStrategy.NONE)
+                .listener(object : RequestListener<Bitmap> {
+                    override fun onLoadFailed(
+                        e: GlideException?,
+                        model: Any?,
+                        target: Target<Bitmap>?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        return false
+                    }
+
+                    override fun onResourceReady(
+                        resource: Bitmap?,
+                        model: Any?,
+                        target: Target<Bitmap>?,
+                        dataSource: DataSource?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        imageView.setImageBitmap(resource)
+                        imageView.visibility = View.VISIBLE
+                        return false
+                    }
+                })
+                .into(imageView)
+        }
     }
 }
