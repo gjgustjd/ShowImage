e5b41e2f6ba11ec43e12f9796f8c9a61b2c5ba0a
gjgustjd <gjgustjd70@naver.com>
2022-04-10 23:11:27 +0900

    Design:액션바 표시 변경
    액션바를 표시하도록 변경
    작가 이름 검색 창 배제
    테마 기본 색상 변경

--- app/src/main/java/com/miso/showimage/model/ImageRepository.kt
+++ app/src/main/java/com/miso/showimage/model/ImageRepository.kt
@@ -1,15 +1,11 @@
 package com.miso.showimage.model
 
-import android.util.Log
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.PagingData
 import com.google.gson.GsonBuilder
 import com.miso.showimage.Common.ShowImagePagingSource
 import kotlinx.coroutines.flow.Flow
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Inject
