31ec7565f4e02aaadb3959043c881091a46f6e56
gjgustjd <gjgustjd70@naver.com>
2022-04-11 03:35:54 +0900

    Refactor:이미지 로딩 속도 향상
    로딩된 이미지 비트맵 형식으로 저장하여 속도 증강

--- app/src/main/java/com/miso/showimage/Activity/PagingImageListAdapter.kt
+++ app/src/main/java/com/miso/showimage/Activity/PagingImageListAdapter.kt
@@ -1,18 +1,24 @@
 package com.miso.showimage.Activity
 
 import android.content.Context
+import android.graphics.Bitmap
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.paging.PagingDataAdapter
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import com.miso.showimage.databinding.ItemImageBinding
 import com.miso.showimage.model.ImageDto
 import dagger.hilt.android.qualifiers.ActivityContext
 
-class PagingImageListAdapter constructor( @ActivityContext private  val context: Context) :
-    PagingDataAdapter<ImageDto, PagingViewHolder>(diffCallback) {
+class PagingImageListAdapter constructor(@ActivityContext private val context: Context) :
+    PagingDataAdapter<ImageDto, PagingImageListAdapter.PagingViewHolder>(diffCallback) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PagingViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
@@ -27,22 +33,6 @@ class PagingImageListAdapter constructor( @ActivityContext private  val context:
             holder.bind(item)
         }
 
-        if (position <= itemCount) {
-            val endPosition = if (position + 6 > itemCount) {
-                itemCount
-            } else {
-                position + 6
-            }
-
-            for (i in position..endPosition-1) {
-                preload(context, getItem(i)!!.download_url)
-            }
-        }
-    }
-
-    fun preload(context: Context, url: String) {
-        Glide.with(context).load(url)
-            .preload()
     }
 
     companion object {
@@ -57,11 +47,12 @@ class PagingImageListAdapter constructor( @ActivityContext private  val context:
 
         }
     }
-}
 
-class PagingViewHolder(private val binding: ItemImageBinding) :
-    RecyclerView.ViewHolder(binding.root) {
-    fun bind(imageDto: ImageDto) {
-        binding.imageDto = imageDto
+    inner class PagingViewHolder(private val binding: ItemImageBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(imageDto: ImageDto) {
+            binding.imageDto = imageDto
+        }
     }
 }
+
--- app/src/main/java/com/miso/showimage/Common/GeneralBindingAdapters.kt
+++ app/src/main/java/com/miso/showimage/Common/GeneralBindingAdapters.kt
@@ -1,34 +1,50 @@
 package com.miso.showimage.Common
 
 import android.graphics.Bitmap
-import android.util.Log
-import android.view.View
 import android.widget.ImageView
-import androidx.annotation.Nullable
-import androidx.core.view.isGone
-import androidx.core.view.isInvisible
-import androidx.core.view.isVisible
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.DataSource
-import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.load.engine.GlideException
 import com.bumptech.glide.request.RequestListener
-import com.bumptech.glide.request.RequestOptions
 import com.bumptech.glide.request.target.Target
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 
 object GeneralBindingAdapters {
+    var bitmaps = HashMap<String, Bitmap?>()
+
     @BindingAdapter("app:imageUrl")
     @JvmStatic
     fun loadImage(imageView: ImageView, url: String) {
-        Glide.with(imageView.context)
-            .load(url)
-            .fitCenter()
-            .thumbnail(0.1f)
-            .diskCacheStrategy(DiskCacheStrategy.ALL)
-            .into(imageView)
+        if (bitmaps.containsKey(url)) {
+            imageView.setImageBitmap(bitmaps.get(url))
+        } else {
+            Glide.with(imageView.context)
+                .asBitmap()
+                .load(url)
+                .thumbnail(0.3f)
+                .fitCenter()
+                .listener(object : RequestListener<Bitmap> {
+                    override fun onLoadFailed(
+                        e: GlideException?,
+                        model: Any?,
+                        target: Target<Bitmap>?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        return false
+                    }
+
+                    override fun onResourceReady(
+                        resource: Bitmap?,
+                        model: Any?,
+                        target: Target<Bitmap>?,
+                        dataSource: DataSource?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        bitmaps.put(url,resource)
+                        return false
+                    }
+                })
+                .into(imageView)
+        }
     }
 }
