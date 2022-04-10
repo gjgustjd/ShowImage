491711806a651d6bc6d5fc12006cbc2e77833ca8
gjgustjd <gjgustjd70@naver.com>
2022-04-10 21:36:04 +0900

    Feat:ViewModel 및 Repository에 Paging3 관련 로직 추가
    ViewModel 및 Repository에 Paging3 로직 추가

--- app/src/main/java/com/miso/showimage/Activity/MainViewModel.kt
+++ app/src/main/java/com/miso/showimage/Activity/MainViewModel.kt
@@ -3,10 +3,16 @@ package com.miso.showimage.Activity
 import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
+import androidx.paging.filter
 import com.miso.showimage.model.ImageDto
 import com.miso.showimage.model.ImageRepository
 import dagger.hilt.android.scopes.ActivityRetainedScoped
 import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 @ActivityRetainedScoped
@@ -17,12 +23,20 @@ class MainViewModel @Inject constructor() : ViewModel() {
     @Inject
     lateinit var imageListLiveData: MutableLiveData<List<ImageDto>>
 
-    fun getImageList(page: Int = 2, limit: Int = 100) {
-        CoroutineScope(Dispatchers.IO).launch{
-            val request = async {
-                repository.getImageList(page, limit)
+    lateinit var pagingData: Flow<PagingData<ImageDto>>
+
+    fun setupPagingData() {
+        pagingData = repository.getPagingData().map {
+            val imageDataMap = mutableSetOf<String>()
+            it.filter { imageDto ->
+                if (imageDataMap .contains(imageDto.id)) {
+                    false
+                } else {
+                    imageDataMap.add(imageDto.id)
+                }
             }
-            imageListLiveData.postValue(request.await())
-        }
+        }.
+        cachedIn(viewModelScope)
     }
+
 }
--- app/src/main/java/com/miso/showimage/model/ImageRepository.kt
+++ app/src/main/java/com/miso/showimage/model/ImageRepository.kt
@@ -1,7 +1,12 @@
 package com.miso.showimage.model
 
 import android.util.Log
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
 import com.google.gson.GsonBuilder
+import com.miso.showimage.Common.ShowImagePagingSource
+import kotlinx.coroutines.flow.Flow
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -26,4 +31,11 @@ class ImageRepository @Inject constructor() {
         val api = retrofit.create(ImageAPI::class.java)
         return api.getImage(page, limit)
     }
+
+    fun getPagingData(): Flow<PagingData<ImageDto>> {
+        return Pager(
+            PagingConfig(pageSize = 10)
+        ) { ShowImagePagingSource(this) }.flow
+    }
+
 }
